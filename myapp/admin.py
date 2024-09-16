2566ad53d35806ecd3df604d8ce32860917b4cb1
SakibSamiul <sb.sakib77@gmail.com>
2024-09-16 16:29:46 +0600

    admin panel

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import ABOUT, SOCIAL, SERVICE, CONTACT
 
 # Register your models here.
+admin.site.register(ABOUT)     
+admin.site.register(SOCIAL)
+admin.site.register(SERVICE)
+admin.site.register(CONTACT)
