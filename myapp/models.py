8f6968662a18bd138151fd022938615f6fdeef1e
SakibSamiul <sb.sakib77@gmail.com>
2024-09-16 16:01:17 +0600

    SOCIAL - models.py

--- myapp/models.py
+++ myapp/models.py
@@ -7,10 +7,15 @@ class ABOUT(models.Model):
     short_description = models.CharField(max_length=300)
     birthday = models.DateField()
     website = models.URLField()
-    phone = models.IntegerField()
+    phone = models.IntegerField(max_length=15)
     city = models.CharField(max_length=100)
     age = models.IntegerField()
     degree = models.CharField(max_length=100)
     email = models.EmailField()
 
+class SOCIAL(models.Model):
+    social_icon = models.CharField(max_length=100)
+    social_link = models.URLField(max_length=1000)
 
+    def __str__(self):
+        return self.social_icon
--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
-from .models import ABOUT
-
+from django.shortcuts import render, redirect
+from .models import ABOUT, SOCIAL, SERVICE
+# from .forms import *
 # Create your views here.
 def Index(request):
-    # social = SOCIAL.objects.all()
-    return render(request, 'index.html')
+    social = SOCIAL.objects.all()
+    return render(request, 'index.html', {'social':social})
 
 def About(request):
     about = ABOUT.objects.get()
@@ -14,7 +14,9 @@ def Resume(request):
     return render(request, 'resume.html')
 
 def Services(request):
-    return render(request, 'services.html')
+    service = SERVICE.objects.all()
+    return render(request, 'services.html', {'service':service})
 
-def Service_details(request):
-    return render(request, 'service-details.html')
+def Service_details(request, pk):
+    service_details = SERVICE.objects.get(pk=pk)
+    return render(request, 'service-details.html', { 'service_details':service_details})
