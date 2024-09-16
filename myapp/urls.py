33caf1fafa59b330c6758f5beb0e4b8a235088db
SakibSamiul <sb.sakib77@gmail.com>
2024-09-16 15:34:48 +0600

    about.html

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . views import home
+from . views import Index, About
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', Index, name= 'index'),
+    path('about', About, name= 'about'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import ABOUT
 
 # Create your views here.
-def home(request):
+def Index(request):
+    # social = SOCIAL.objects.all()
     return render(request, 'index.html')
+
+def About(request):
+    # about = ABOUT.objects.get()
+    return render(request, 'about.html')
